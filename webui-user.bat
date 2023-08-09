c543f268b00986686aa07dd68ebc66aafffc8848
sillyL12324 <liushun0574@gmail.com>
2023-08-09 16:34:08 +0800

    add extension

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --no-download-sd-model --xformers --enable-insecure-extension-access --share --gradio-auth admin:123456
 
 call webui.bat
